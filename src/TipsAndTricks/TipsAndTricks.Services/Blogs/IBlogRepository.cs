9ab6a16567cb11b58d3ed68e54814dc262bc411c
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-02-24 16:36:52 +0700

    Add query methods

---
+++ src/TipsAndTricks/TipsAndTricks.Services/Blogs/IBlogRepository.cs
@@ -0,0 +1,10 @@
+﻿using TipsAndTricks.Core.Entities;
+
+namespace TipsAndTricks.Services.Blogs {
+    public interface IBlogRepository {
+        Task<Post> GetPostAsync(int year, int month, string slug, CancellationToken cancellationToken = default);
+        Task<IList<Post>> GetPopularArticlesAsync(int numPosts, CancellationToken cancellationToken = default);
+        Task<bool> IsPostPlugExistedAsync(int postId, string slug, CancellationToken cancellationToken = default);
+        Task IncreaseViewCountAsync(int postId, CancellationToken cancellationToken = default);
+    }
+}
--- src/TipsAndTricks/TipsAndTricks.WinApp/Program.cs
+++ src/TipsAndTricks/TipsAndTricks.WinApp/Program.cs
@@ -1,25 +1,16 @@
 ﻿using TipsAndTricks.Data.Contexts;
+using TipsAndTricks.Services.Blogs;
 
 var context = new BlogDbContext();
-var posts = context.Posts
-    .Where(p => p.Published)
-    .OrderBy(p => p.Title)
-    .Select(p => new {
-        Id = p.Id,
-        Title = p.Title,
-        ViewCount = p.ViewCount,
-        PostedDate = p.PostedDate,
-        Author = p.Author.FullName,
-        Category = p.Category.Name
-    })
-    .ToList();
+IBlogRepository blogRepository = new BlogRepository(context);
+var posts = await blogRepository.GetPopularArticlesAsync(3);
 
 foreach (var post in posts) {
     Console.WriteLine("ID       : {0}", post.Id);
     Console.WriteLine("Title    : {0}", post.Title);
     Console.WriteLine("View     : {0}", post.ViewCount);
     Console.WriteLine("Date     : {0:dd/MM/yyyy}", post.PostedDate);
-    Console.WriteLine("Author   : {0}", post.Author);
-    Console.WriteLine("Category : {0}", post.Category);
+    Console.WriteLine("Author   : {0}", post.Author.FullName);
+    Console.WriteLine("Category : {0}", post.Category.Name);
     Console.WriteLine("".PadRight(80, '-'));
 }
