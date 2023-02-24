587391b5bfd99b2261ed4b76b06ba361140dcd71
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-02-24 16:01:32 +0700

    Test data

--- src/TipsAndTricks/TipsAndTricks.WinApp/Program.cs
+++ src/TipsAndTricks/TipsAndTricks.WinApp/Program.cs
@@ -1,14 +1,25 @@
 ﻿using TipsAndTricks.Data.Contexts;
-using TipsAndTricks.Data.Seeders;
 
 var context = new BlogDbContext();
-var seeder = new DataSeeder(context);
+var posts = context.Posts
+    .Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author = p.Author.FullName,
+        Category = p.Category.Name
+    })
+    .ToList();
 
-seeder.Initialize();
-
-var authors = context.Authors.ToList();
-
-Console.WriteLine("{0, -4}{1, -30}{2, -30}{3, 12}", "ID", "Full Name", "Email", "Joined Date");
-foreach (var author in authors) {
-    Console.WriteLine("{0, -4}{1, -30}{2, -30}{3, 12}", author.Id, author.FullName, author.Email, author.JoinedDate);
+foreach (var post in posts) {
+    Console.WriteLine("ID       : {0}", post.Id);
+    Console.WriteLine("Title    : {0}", post.Title);
+    Console.WriteLine("View     : {0}", post.ViewCount);
+    Console.WriteLine("Date     : {0:dd/MM/yyyy}", post.PostedDate);
+    Console.WriteLine("Author   : {0}", post.Author);
+    Console.WriteLine("Category : {0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
 }
