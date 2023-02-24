30236dd2a128801d412a6a878e74aacb0f323c03
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-02-24 14:37:46 +0700

    Create database

---
+++ src/TipsAndTricks/TipsAndTricks.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TipsAndTricks.Core.Entities;
+using TipsAndTricks.Data.Mappings;
+
+namespace TipsAndTricks.Data.Contexts {
+    public class BlogDbContext : DbContext {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-52H2UAJ;Database=TipsAndTricksBlog;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryMap).Assembly);
+        }
+    }
+}
