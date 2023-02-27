6068b35289234bca6ae9ce90bd02716e76d12e4d
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-02-27 13:59:52 +0700

    Add pagination

---
+++ src/TipsAndTricks/TipsAndTricks.Core/DTO/TagItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TipsAndTricks.Core.DTO {
+    public class TagItem {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UrlSlug { get; set; }
+        public string Description { get; set; }
+        public int PostCount { get; set; }
+    }
+}
--- src/TipsAndTricks/TipsAndTricks.Services/Blogs/IBlogRepository.cs
+++ src/TipsAndTricks/TipsAndTricks.Services/Blogs/IBlogRepository.cs
@@ -1,4 +1,5 @@
-﻿using TipsAndTricks.Core.DTO;
+﻿using TipsAndTricks.Core.Contracts;
+using TipsAndTricks.Core.DTO;
 using TipsAndTricks.Core.Entities;
 
 namespace TipsAndTricks.Services.Blogs {
@@ -8,5 +9,6 @@ namespace TipsAndTricks.Services.Blogs {
         Task<bool> IsPostPlugExistedAsync(int postId, string slug, CancellationToken cancellationToken = default);
         Task IncreaseViewCountAsync(int postId, CancellationToken cancellationToken = default);
         Task<IList<CategoryItem>> GetCategoriesAsync(bool showOnMenu = false, CancellationToken cancellationToken = default);
+        Task<IPagedList<TagItem>> GetPagedTagsAsync(IPagingParams pagingParams, CancellationToken cancellationToken = default);
     }
 }
