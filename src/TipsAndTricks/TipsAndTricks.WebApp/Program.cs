462a4678874e98a4acdc8e07e86bfcf35765a8f3
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-06 18:24:28 +0700

    Add services and config request pipeline

--- src/TipsAndTricks/TipsAndTricks.WebApp/Program.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApp/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); {
+    builder.Services.AddControllersWithViews();
+}
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build(); {
+    if (app.Environment.IsDevelopment())
+        app.UseDeveloperExceptionPage();
+    else {
+        app.UseExceptionHandler("/Blog/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.MapControllerRoute(name: "default", pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
 
 app.Run();
