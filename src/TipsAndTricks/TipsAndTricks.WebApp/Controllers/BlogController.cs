5ec11234680c6b2805136570a31085fa465c4157
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-06 18:16:52 +0700

    Add controller

---
+++ src/TipsAndTricks/TipsAndTricks.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TipsAndTricks.WebApp.Controllers {
+    public class BlogController : Controller {
+        public IActionResult Index() {
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+
+            return View();
+        }
+
+        public IActionResult About() => View();
+
+        public IActionResult Contact() => View();
+
+        public IActionResult Rss() => Content("Nội dung sẽ được cập nhật");
+    }
+}
