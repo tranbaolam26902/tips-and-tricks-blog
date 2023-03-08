2ea8af50f2a8c1e62895f491f9c56b0bfc1edcc1
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-08 22:06:55 +0700

    Add Archives Widget

---
+++ src/TipsAndTricks/TipsAndTricks.WebApp/Components/ArchivesWidget.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipsAndTricks.Services.Blogs;
+
+namespace TipsAndTricks.WebApp.Components {
+    public class ArchivesWidget : ViewComponent {
+        private readonly IBlogRepository _blogRepository;
+
+        public ArchivesWidget(IBlogRepository blogRepository) {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var posts = await _blogRepository.CountMonthlyPostsAsync(12);
+
+            return View(posts);
+        }
+    }
+}
