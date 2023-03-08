7f93b6e3c5a0a4853f5a2ca3681a05e47a5ee0bf
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-08 21:43:46 +0700

    Add Featured Posts Widget

---
+++ src/TipsAndTricks/TipsAndTricks.WebApp/Components/FeaturedPostsWidget.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipsAndTricks.Services.Blogs;
+
+namespace TipsAndTricks.WebApp.Components {
+    public class FeaturedPostsWidget : ViewComponent {
+        private readonly IBlogRepository _blogRepository;
+
+        public FeaturedPostsWidget(IBlogRepository blogRepository) {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var posts = await _blogRepository.GetPopularArticlesAsync(3);
+
+            return View(posts);
+        }
+    }
+}
