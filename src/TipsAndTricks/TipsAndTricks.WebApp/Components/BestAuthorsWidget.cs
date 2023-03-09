7d7b2fe2afe7278cc4e9703bc92fa6113d828b80
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-09 23:21:30 +0700

    Add Best Authors section

---
+++ src/TipsAndTricks/TipsAndTricks.WebApp/Components/BestAuthorsWidget.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipsAndTricks.Services.Blogs;
+
+namespace TipsAndTricks.WebApp.Components {
+    public class BestAuthorsWidget : ViewComponent {
+        private readonly IAuthorRepository _authorRepository;
+
+        public BestAuthorsWidget(IAuthorRepository authorRepository) {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var authors = await _authorRepository.GetAuthorsHasMostArticles(4);
+
+            return View(authors);
+        }
+    }
+}
