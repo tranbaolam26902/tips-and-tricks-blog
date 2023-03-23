66fdf48dc74fc37799dfc15ad345c4a2e7326394
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-23 13:51:23 +0700

    Add GET categories api

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CategoryEndpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CategoryEndpoints.cs
@@ -1,9 +1,29 @@
-﻿namespace TipsAndTricks.WebApi.Endpoints {
+﻿using TipsAndTricks.Core.Collections;
+using TipsAndTricks.Core.DTO;
+using TipsAndTricks.Services.Blogs;
+using TipsAndTricks.Services.FilterParams;
+using TipsAndTricks.WebApi.Models.Categories;
+
+namespace TipsAndTricks.WebApi.Endpoints {
     public static class CategoryEndpoints {
         public static WebApplication MapCategoryEndpoints(this WebApplication app) {
             var routeGroupBuilder = app.MapGroup("/api/categories");
 
+            routeGroupBuilder.MapGet("/", GetCategories)
+               .WithName("GetCategories")
+               .Produces<PaginationResult<CategoryItem>>();
+
             return app;
+        }
+
+        private static async Task<IResult> GetCategories([AsParameters] CategoryFilterModel model, IBlogRepository blogRepository) {
+            var categoryQuery = new CategoryQuery() {
+                Keyword = model.Name
+            };
+            var categories = await blogRepository.GetPagedCategoriesByQueryAsync(categoryQuery, model);
+            var paginationResult = new PaginationResult<CategoryItem>(categories);
+
+            return Results.Ok(paginationResult);
         }
     }
 }
