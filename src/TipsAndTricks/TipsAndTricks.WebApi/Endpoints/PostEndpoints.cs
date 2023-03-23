01c1674bd5193d187ebe3ddb8605fab8bee8805a
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-23 15:53:17 +0700

    Add GET random posts api

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/PostEndpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/PostEndpoints.cs
@@ -16,6 +16,9 @@ namespace TipsAndTricks.WebApi.Endpoints {
             routeGroupBuilder.MapGet("/featured/{limit:int}", GetPopularPosts)
                 .WithName("GetPopularPosts")
                 .Produces<IList<PostDTO>>();
+            routeGroupBuilder.MapGet("/random/{limit:int}", GetRandomPosts)
+                .WithName("GetRandomPosts")
+                .Produces<IList<PostDTO>>();
 
             return app;
         }
@@ -29,6 +32,12 @@ namespace TipsAndTricks.WebApi.Endpoints {
 
         public static async Task<IResult> GetPopularPosts(int limit, IBlogRepository blogRepository, IMapper mapper) {
             var posts = await blogRepository.GetPopularArticlesAsync(limit);
+
+            return posts.Count != 0 ? Results.Ok(mapper.Map<IList<PostDTO>>(posts)) : Results.NotFound("Không có bài viết");
+        }
+
+        public static async Task<IResult> GetRandomPosts(int limit, IBlogRepository blogRepository, IMapper mapper) {
+            var posts = await blogRepository.GetRandomPostsAsync(limit);
 
             return posts.Count != 0 ? Results.Ok(mapper.Map<IList<PostDTO>>(posts)) : Results.NotFound("Không có bài viết");
         }
