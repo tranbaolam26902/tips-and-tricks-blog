bfb0c8fed3ea8ce4e8fc20f6d91655626bbc8c0b
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-25 12:06:06 +0700

    Add tag endpoints

---
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/TagEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace TipsAndTricks.WebApi.Endpoints {
+    public static class TagEndpoints {
+        public static WebApplication MapTagEndPoints(this WebApplication app) {
+            var routeGroupBuilder = app.MapGroup("/api/tags");
+
+            return app;
+        }
+    }
+}
