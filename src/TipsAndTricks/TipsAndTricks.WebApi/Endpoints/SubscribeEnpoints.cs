69a2d731d32bf7b20d7128993a2d8fc8c5981fd1
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-25 16:12:17 +0700

    Add POST subscribe/unsubscribe by email api

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/SubscribeEnpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/SubscribeEnpoints.cs
@@ -8,16 +8,24 @@ using TipsAndTricks.WebApi.Models.Subscribers;
 namespace TipsAndTricks.WebApi.Endpoints {
     public static class SubscribeEnpoints {
         public static WebApplication MapSubscribeEndpoints(this WebApplication app) {
-            var routeGroupBuilder = app.MapGroup("/api/subscribe");
+            var routeGroupBuilder = app.MapGroup("/api/subscribers");
 
             routeGroupBuilder.MapGet("/", GetSubscribers)
                 .WithName("GetSubscribers")
                 .Produces<IPagedList<Subscriber>>();
-            routeGroupBuilder.MapPut("/{id:int}", BanSubscriber)
+            routeGroupBuilder.MapPut("/{id:int}/ban", BanSubscriber)
                 .WithName("BanSubscriber")
                 .Produces(404);
-            routeGroupBuilder.MapDelete("/{id:int}", DeleteSubscriber)
+            routeGroupBuilder.MapDelete("/{id:int}/unban", DeleteSubscriber)
                 .WithName("DeleteSubscriber")
+                .Produces(204)
+                .Produces(404);
+            routeGroupBuilder.MapPost("/{email:regex(^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{{2,3}}$)}/subscribe", Subscribe)
+                .WithName("Subscribe")
+                .Produces(204)
+                .ProducesProblem(400);
+            routeGroupBuilder.MapPost("/{email:regex(^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{{2,3}}$)}/unsubscribe", Unsubscribe)
+                .WithName("Unsubscribe")
                 .Produces(204)
                 .Produces(404);
 
@@ -47,6 +55,27 @@ namespace TipsAndTricks.WebApi.Endpoints {
             return await subscriberRepository.DeleteSubscriberByIdAsync(id)
                 ? Results.NoContent()
                 : Results.NotFound($"Không tìm thấy người theo dõi có mã số {id}");
+        }
+
+        private static async Task<IResult> Subscribe(string email, ISubscriberRepository subscriberRepository) {
+            var isSuccess = await subscriberRepository.SubscribeAsync(email);
+
+            if (!isSuccess) {
+                var subscriber = await subscriberRepository.GetSubscriberByEmailAsync(email);
+                if (subscriber.SubscribeState == SubscribeState.Banned)
+                    return Results.Problem($"Email {email} này đã bị cấm bởi quản trị viên!");
+                if (subscriber.SubscribeState == SubscribeState.Subscribe) {
+                    return Results.Problem($"Email {email} đã đăng ký nhận thông báo trước đó!");
+                }
+            }
+
+            return Results.NoContent();
+        }
+
+        private static async Task<IResult> Unsubscribe(string email, UnsubscribeEditModel model, ISubscriberRepository subscriberRepository) {
+            return await subscriberRepository.UnsubscribeAsync(email, model.Reason)
+                ? Results.NoContent()
+                : Results.NotFound($"Email {email} chưa đăng ký nhận thông báo!");
         }
     }
 }
---
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Models/Subscribers/UnsubscribeEditModel.cs
@@ -0,0 +1,5 @@
+﻿namespace TipsAndTricks.WebApi.Models.Subscribers {
+    public class UnsubscribeEditModel {
+        public string Reason { get; set; }
+    }
+}
