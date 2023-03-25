5d6c9ff2c507e32ab077fb0792976d8717ce12f5
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-25 16:28:25 +0700

    Add GET comments api

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CommentEndpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CommentEndpoints.cs
@@ -1,9 +1,26 @@
-﻿namespace TipsAndTricks.WebApi.Endpoints {
+﻿using TipsAndTricks.Core.Collections;
+using TipsAndTricks.Core.Entities;
+using TipsAndTricks.Services.Blogs;
+using TipsAndTricks.Services.FilterParams;
+using TipsAndTricks.WebApi.Models;
+
+namespace TipsAndTricks.WebApi.Endpoints {
     public static class CommentEndpoints {
         public static WebApplication MapCommentEndpoints(this WebApplication app) {
             var routeGroupBuilder = app.MapGroup("/api/comments");
 
+            routeGroupBuilder.MapGet("/", GetComments)
+                .WithName("GetComments")
+                .Produces<PaginationResult<Comment>>();
+
             return app;
+        }
+
+        private static async Task<IResult> GetComments([AsParameters] CommentQuery query, [AsParameters] PagingModel pagingModel, ICommentRepository commentRepository) {
+            var comments = await commentRepository.GetPagedCommentsByQueryAsync(query, pagingModel);
+            var paginationResult = new PaginationResult<Comment>(comments);
+
+            return Results.Ok(paginationResult);
         }
     }
 }
