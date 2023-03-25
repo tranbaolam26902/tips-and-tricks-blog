8be1c629b515c5348c6f1d341819fd42f9a9a2af
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-25 13:49:34 +0700

    Add subscribe endpoints

---
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/SubscribeEnpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace TipsAndTricks.WebApi.Endpoints {
+    public static class SubscribeEnpoints {
+        public static WebApplication MapSubscribeEndpoints(this WebApplication app) {
+            var routeGroupBuilder = app.MapGroup("/api/subscribe");
+
+            return app;
+        }
+    }
+}
