9b63b1a03f2e17df807fcf45ac1e5cc95b3ece40
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-25 13:10:03 +0700

    Add POST & PUT tag api

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/TagEndpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/TagEndpoints.cs
@@ -3,8 +3,10 @@ using MapsterMapper;
 using Microsoft.AspNetCore.Mvc;
 using TipsAndTricks.Core.Collections;
 using TipsAndTricks.Core.DTO;
+using TipsAndTricks.Core.Entities;
 using TipsAndTricks.Services.Blogs;
 using TipsAndTricks.Services.FilterParams;
+using TipsAndTricks.WebApi.Filter;
 using TipsAndTricks.WebApi.Models;
 using TipsAndTricks.WebApi.Models.Posts;
 using TipsAndTricks.WebApi.Models.Tags;
@@ -24,6 +26,18 @@ namespace TipsAndTricks.WebApi.Endpoints {
             routeGroupBuilder.MapGet("/{slug:regex(^[a-z0-9_-]+$)}/posts", GetTagPostsBySlug)
                 .WithName("GetTagPostsBySlug")
                 .Produces<PaginationResult<PostDTO>>();
+            routeGroupBuilder.MapPost("/", AddTag)
+                .WithName("AddNewTag")
+                .AddEndpointFilter<ValidatorFilter<TagEditModel>>()
+                .Produces(201)
+                .Produces(400)
+                .Produces(409);
+            routeGroupBuilder.MapPut("/{id:int}", UpdateTag)
+                .WithName("UpdateATag")
+                .AddEndpointFilter<ValidatorFilter<TagEditModel>>()
+                .Produces(201)
+                .Produces(400)
+                .Produces(409);
 
             return app;
         }
@@ -55,6 +69,30 @@ namespace TipsAndTricks.WebApi.Endpoints {
             var paginationResult = new PaginationResult<PostDTO>(posts);
 
             return Results.Ok(paginationResult);
+        }
+
+        private static async Task<IResult> AddTag(TagEditModel model, IBlogRepository blogRepository, IMapper mapper) {
+            if (await blogRepository.IsTagSlugExistedAsync(0, model.UrlSlug)) {
+                return Results.Conflict($"Slug '{model.UrlSlug}' đã được sử dụng");
+            }
+
+            var tag = mapper.Map<Tag>(model);
+            await blogRepository.EditTagAsync(tag);
+
+            return Results.CreatedAtRoute("GetTagById", new { tag.Id }, mapper.Map<TagItem>(tag));
+        }
+
+        private static async Task<IResult> UpdateTag(int id, TagEditModel model, IBlogRepository blogRepository, IMapper mapper) {
+            if (await blogRepository.IsTagSlugExistedAsync(id, model.UrlSlug)) {
+                return Results.Conflict($"Slug '{model.UrlSlug}' đã được sử dụng");
+            }
+
+            var tag = mapper.Map<Tag>(model);
+            tag.Id = id;
+
+            return await blogRepository.EditTagAsync(tag) != null
+                ? Results.NoContent()
+                : Results.NotFound();
         }
     }
 }
---
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Models/Tags/TagEditModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TipsAndTricks.WebApi.Models.Tags {
+    public class TagEditModel {
+        public string Name { get; set; }
+        public string UrlSlug { get; set; }
+        public string Description { get; set; }
+    }
+}
