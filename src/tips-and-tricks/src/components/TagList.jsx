a1454b580b5eae55067c16e29fc967d28d7fab57
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 22:26:40 +0700

    Add post default thumbnail

---
+++ src/tips-and-tricks/src/components/TagList.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+export default function TagList({ tags }) {
+	return (
+		<>
+			{tags && Array.isArray(tags) && tags.length > 0
+				? tags.map((tag, index) => (
+						<Link
+							key={index}
+							to={`/blog/tag?slug=${tag.name}`}
+							title={tag.name}
+							className='btn btn-sm btn-outline-secondary me-1'
+						>
+							{tag.name}
+						</Link>
+				  ))
+				: null}
+		</>
+	);
+}
