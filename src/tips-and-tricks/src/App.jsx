d8503b9aab714d193036a0ab7a448f003be7b8e3
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 18:50:34 +0700

    Add sidebar

--- src/tips-and-tricks/src/App.jsx
+++ src/tips-and-tricks/src/App.jsx
@@ -3,11 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Navigation from './components/Navigation';
+import Sidebar from './components/Sidebar';
 
 function App() {
 	return (
 		<Router>
 			<Navigation />
+			<div className='container-fluid'>
+				<div className='row'>
+					<div className='col-9'></div>
+					<div className='col-3 border-start'>
+						<Sidebar />
+					</div>
+				</div>
+			</div>
 		</Router>
 	);
 }
