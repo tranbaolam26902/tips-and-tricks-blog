e392f72841dce3eba8a81393a941bfc1e0638f13
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 23:35:15 +0700

    Add categories widget

--- src/tips-and-tricks/src/components/Sidebar.jsx
+++ src/tips-and-tricks/src/components/Sidebar.jsx
@@ -1,10 +1,11 @@
 import SearchForm from './SearchForm';
+import CategoriesWidget from './CategoriesWidget';
 
 export default function Sidebar() {
 	return (
 		<div className='pt-4 ps-2'>
 			<SearchForm />
-			<h1>Các chủ đề</h1>
+			<CategoriesWidget />
 			<h1>Bài viết nổi bật</h1>
 			<h1>Đăng ký nhận tin mới</h1>
 			<h1>Tag cloud</h1>
