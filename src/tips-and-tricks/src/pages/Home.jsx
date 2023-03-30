f7be6c2819ee6888a010caa90feb6982d305fd79
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 22:47:26 +0700

    Get posts from api

--- src/tips-and-tricks/src/pages/Home.jsx
+++ src/tips-and-tricks/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import PostItem from '../components/PostItem';
+import { getPosts } from '../services/blogRepository';
 
 export default function Home() {
 	// Component's states
@@ -8,16 +9,25 @@ export default function Home() {
 
 	useEffect(() => {
 		document.title = 'Trang chủ';
+		fetchPosts();
+
+		async function fetchPosts() {
+			const data = await getPosts();
+			if (data) setPosts(data.items);
+			else setPosts([]);
+		}
 	}, []);
 
 	return (
 		<>
-			{posts.length > 0 && (
+			{posts.length > 0 ? (
 				<div className='p-4'>
 					{posts.map((post) => (
 						<PostItem key={post.id} post={post} />
 					))}
 				</div>
+			) : (
+				<h1 className='text-danger'>Không có bài viết</h1>
 			)}
 		</>
 	);
