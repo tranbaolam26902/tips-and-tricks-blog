a1fb385f146f14d07e02bdbd99a138ec2c80b89a
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-30 17:22:45 +0700

    Use Api Response for category endpoint

--- src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CategoryEndpoints.cs
+++ src/TipsAndTricks/TipsAndTricks.WebApi/Endpoints/CategoryEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using Mapster;
 using MapsterMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 using TipsAndTricks.Core.Collections;
 using TipsAndTricks.Core.DTO;
 using TipsAndTricks.Core.Entities;
@@ -18,30 +19,32 @@ namespace TipsAndTricks.WebApi.Endpoints {
 
             routeGroupBuilder.MapGet("/", GetCategories)
                 .WithName("GetCategories")
-                .Produces<PaginationResult<CategoryItem>>();
+                .Produces<ApiResponse<PaginationResult<CategoryItem>>>();
+
             routeGroupBuilder.MapGet("/{id:int}", GetCategoryById)
                 .WithName("GetCategoryById")
-                .Produces<CategoryItem>()
-                .Produces(404);
+                .Produces<ApiResponse<CategoryItem>>();
+
             routeGroupBuilder.MapGet("/{slug:regex(^[a-z0-9_-]+$)}/posts", GetPostsByCategorySlug)
                 .WithName("GetPostsByCategorySlug")
-                .Produces<PaginationResult<PostDTO>>();
+                .Produces<ApiResponse<PaginationResult<PostDTO>>>();
+
             routeGroupBuilder.MapPost("/", AddCategory)
                 .WithName("AddNewCategory")
                 .AddEndpointFilter<ValidatorFilter<CategoryEditModel>>()
-                .Produces(201)
-                .Produces(400)
-                .Produces(409);
+                .Produces(401)
+                .Produces<ApiResponse<CategoryItem>>();
+
             routeGroupBuilder.MapPut("/{id:int}", UpdateCategory)
                 .WithName("UpdateAnCategory")
                 .AddEndpointFilter<ValidatorFilter<CategoryEditModel>>()
-                .Produces(204)
-                .Produces(400)
-                .Produces(409);
+                .Produces(401)
+                .Produces<ApiResponse<string>>();
+
             routeGroupBuilder.MapDelete("/{id:int}", DeleteCategory)
                 .WithName("DeleteCategory")
-                .Produces(204)
-                .Produces(404);
+                .Produces(401)
+                .Produces<ApiResponse<string>>();
 
             return app;
         }
@@ -53,15 +56,15 @@ namespace TipsAndTricks.WebApi.Endpoints {
             var categories = await blogRepository.GetPagedCategoriesByQueryAsync(categoryQuery, model);
             var paginationResult = new PaginationResult<CategoryItem>(categories);
 
-            return Results.Ok(paginationResult);
+            return Results.Ok(ApiResponse.Success(paginationResult));
         }
 
         private static async Task<IResult> GetCategoryById(int id, IBlogRepository blogRepository, IMapper mapper) {
             var category = await blogRepository.GetCategoryByIdAsync(id);
 
             return category == null
-                ? Results.NotFound($"Không tìm thấy chủ đề có mã số {id}")
-                : Results.Ok(mapper.Map<CategoryItem>(category));
+                ? Results.Ok(ApiResponse.Fail(HttpStatusCode.NotFound, $"Không tìm thấy chủ đề có mã số {id}"))
+                : Results.Ok(ApiResponse.Success(mapper.Map<CategoryItem>(category)));
         }
 
         private static async Task<IResult> GetPostsByCategorySlug([FromRoute] string slug, [AsParameters] PagingModel pagingModel, IBlogRepository blogRepository) {
@@ -72,37 +75,37 @@ namespace TipsAndTricks.WebApi.Endpoints {
             var posts = await blogRepository.GetPagedPostsByQueryAsync(posts => posts.ProjectToType<PostDTO>(), postQuery, pagingModel);
             var paginationResult = new PaginationResult<PostDTO>(posts);
 
-            return Results.Ok(paginationResult);
+            return Results.Ok(ApiResponse.Success(paginationResult));
         }
 
         private static async Task<IResult> AddCategory(CategoryEditModel model, IBlogRepository blogRepository, IMapper mapper) {
             if (await blogRepository.IsCategorySlugExistedAsync(0, model.UrlSlug)) {
-                return Results.Conflict($"Slug '{model.UrlSlug}' đã được sử dụng");
+                return Results.Ok(ApiResponse.Fail(HttpStatusCode.Conflict, $"Slug '{model.UrlSlug}' đã được sử dụng"));
             }
 
             var category = mapper.Map<Category>(model);
             await blogRepository.EditCategoryAsync(category);
 
-            return Results.CreatedAtRoute("GetCategoryById", new { category.Id }, mapper.Map<CategoryItem>(category));
+            return Results.Ok(ApiResponse.Success(mapper.Map<CategoryItem>(category), HttpStatusCode.Created));
         }
 
         private static async Task<IResult> UpdateCategory(int id, CategoryEditModel model, IBlogRepository blogRepository, IMapper mapper) {
             if (await blogRepository.IsCategorySlugExistedAsync(id, model.UrlSlug)) {
-                return Results.Conflict($"Slug '{model.UrlSlug}' đã được sử dụng");
+                return Results.Ok(ApiResponse.Fail(HttpStatusCode.Conflict, $"Slug '{model.UrlSlug}' đã được sử dụng"));
             }
 
             var category = mapper.Map<Category>(model);
             category.Id = id;
 
             return await blogRepository.EditCategoryAsync(category) != null
-                ? Results.NoContent()
-                : Results.NotFound();
+                ? Results.Ok(ApiResponse.Success("Cập nhật thành công", HttpStatusCode.NoContent))
+                : Results.Ok(ApiResponse.Fail(HttpStatusCode.NotFound, $"Không thể tìm thấy chủ đề có mã số {id}"));
         }
 
         private static async Task<IResult> DeleteCategory(int id, IBlogRepository blogRepository) {
             return await blogRepository.DeleteCategoryByIdAsync(id)
-                ? Results.NoContent()
-                : Results.NotFound($"Không tìm thấy chủ đề có mã số {id}");
+                ? Results.Ok(ApiResponse.Success("Xóa thành công", HttpStatusCode.NoContent))
+                : Results.Ok(ApiResponse.Fail(HttpStatusCode.NotFound, $"Không thể tìm thấy chủ đề có mã số {id}"));
         }
     }
 }
