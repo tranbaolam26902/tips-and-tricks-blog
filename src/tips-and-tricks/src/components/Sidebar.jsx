8a03756e4c1c88720a5e22ce699ec2f2de4f49e3
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-31 10:58:51 +0700

    Rename component

--- src/tips-and-tricks/src/components/Sidebar.jsx
+++ src/tips-and-tricks/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import SearchForm from './SearchForm';
-import { CategoriesWidget, FeaturedPosts } from './widgets';
+import { CategoriesWidget, FeaturedPostsWidget } from './widgets';
 
 export default function Sidebar() {
 	return (
 		<div className='pt-4 ps-2'>
 			<SearchForm />
 			<CategoriesWidget />
-			<FeaturedPosts />
+			<FeaturedPostsWidget />
 			<h1>Đăng ký nhận tin mới</h1>
 			<h1>Tag cloud</h1>
 		</div>
