000b4275bfe4e2a4decb99575c13b2ef6da33efe
tranbaolam26902 <tranbaolam26902@gmail.com>
2023-03-31 12:18:15 +0700

    Update about page content

--- src/tips-and-tricks/src/pages/blog/About.jsx
+++ src/tips-and-tricks/src/pages/blog/About.jsx
@@ -1,3 +1,41 @@
 export default function About() {
-	return <h1>About</h1>;
+	return (
+		<div className='p-5'>
+			<h2 className='mb-5 text-uppercase text-center'>Giới thiệu</h2>
+			<iframe
+				className='w-100'
+				height='640'
+				src='https://www.youtube.com/embed/dBDBq7FjogM'
+				title='Zoolander meme BlvckVines x Kenjumboy.'
+				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+				allowFullScreen
+			></iframe>
+			<div className='mt-5'>
+				<div className='d-flex justify-content-between row gx-4'>
+					<img
+						src='/assets/images/about-01.jpg'
+						className='w-50'
+						alt='about-image'
+					/>
+					<img
+						src='/assets/images/about-02.jpg'
+						className='w-50'
+						alt='about-image'
+					/>
+				</div>
+				<div className='d-flex justify-content-between row gx-4 mt-4'>
+					<img
+						src='/assets/images/about-03.jpg'
+						className='w-50'
+						alt='about-image'
+					/>
+					<img
+						src='/assets/images/about-04.jpg'
+						className='w-50'
+						alt='about-image'
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
