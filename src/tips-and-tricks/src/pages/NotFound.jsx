7327d9b0e247ab9eae1a07c02a84fb3ea5d15342
2011401 <2011401@dlu.edu.vn>
2023-04-06 21:58:44 +0700

    Revert "Edit not found page"
    This reverts commit 6fd00c2b7728c5aa134d9a5a3df172c2e4373079.

--- src/tips-and-tricks/src/pages/NotFound.jsx
+++ src/tips-and-tricks/src/pages/NotFound.jsx
@@ -1,8 +1,6 @@
-import { Link } from 'react-router-dom';
-
 export default function NotFound() {
 	return (
-		<div className='d-flex flex-column align-items-center mt-5 pt-5'>
+		<div className='d-flex flex-column align-items-center mt-5'>
 			<img
 				src='/assets/images/error.jpg'
 				alt='not-found'
@@ -10,10 +8,7 @@ export default function NotFound() {
 				style={{ width: 512 }}
 			/>
 			<h1 className='mt-4 fw-bold'>404</h1>
-			<p className='mb-3 fs-3'>Không tìm thấy trang</p>
-			<Link to='/' className='btn btn-primary'>
-				Quay về trang chủ
-			</Link>
+			<p className='fs-3'>Không tìm thấy trang</p>
 		</div>
 	);
 }
