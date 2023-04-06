cc386b5148d2d3249d1dd78abd1ccd690f58db6f
2011401 <2011401@dlu.edu.vn>
2023-04-06 18:43:43 +0700

    Fix footer background color and copyright icon

--- src/tips-and-tricks/src/components/blog/Footer.jsx
+++ src/tips-and-tricks/src/components/blog/Footer.jsx
@@ -3,8 +3,8 @@ import styles from '../../styles/layout.module.css';
 export default function Footer() {
 	return (
 		<footer className={`${styles.footer} border-top text-muted`}>
-			<div className='container-fluid text-center'>
-				@copy; 2023 - Cats & Tricks Blog
+			<div className='container-fluid text-center bg-white'>
+				&#169; 2023 - Cats & Tricks Blog
 			</div>
 		</footer>
 	);
